2839f2d4d61831b62bd53635cd55c52b1ae79987
easyhz <jiton3@naver.com>
2024-02-16 17:32:14 +0900

    Update: Apply Gradle version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,12 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures{
+        dataBinding{
+            enable = true
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -39,16 +45,16 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 
 
     // dagger hilt
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-compiler:2.43.2")
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
 }
