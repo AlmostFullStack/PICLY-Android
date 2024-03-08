d72fa724efb23598ccfece2ad14252240f012254
easyhz <jiton3@naver.com>
2024-03-08 12:13:39 +0900

    update: 스플래시 화면 추가

--- app/src/main/java/com/easyhz/picly/MainActivity.kt
+++ app/src/main/java/com/easyhz/picly/MainActivity.kt
@@ -9,29 +9,23 @@ import com.easyhz.picly.databinding.ActivityMainBinding
 import com.easyhz.picly.view.MainViewModel
 import com.easyhz.picly.view.navigation.NavControllerManager
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
-        observeIsFirstRun()
+        getFirstRun()
         setContentView(binding.root)
     }
 
-    private fun observeIsFirstRun() {
-        viewModel.isFirstRun.observe(this) { isFirstRun ->
-            println("제발 요.. $isFirstRun")
-            initNavControllerManager(isFirstRun)
-            if (isFirstRun) {
-                viewModel.updateFirstRunStatus()
-            }
-        }
+    private fun getFirstRun() {
+        val isFirstRun = intent.getBooleanExtra("isFirstRun", true)
+        initNavControllerManager(isFirstRun)
     }
-
     private fun initNavControllerManager(isFirstRun: Boolean) {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
         NavControllerManager.init(navHostFragment.navController, isFirstRun)
---
+++ app/src/main/java/com/easyhz/picly/view/StartActivity.kt
@@ -0,0 +1,46 @@
+package com.easyhz.picly.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.easyhz.picly.MainActivity
+import com.easyhz.picly.databinding.ActivityStartBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class StartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        observeIsFirstRun()
+        setContentView(binding.root)
+    }
+
+    private fun observeIsFirstRun() {
+        viewModel.isFirstRun.observe(this) { isFirstRun ->
+            if (isFirstRun) {
+                viewModel.updateFirstRunStatus()
+            }
+            CoroutineScope(Dispatchers.Main).launch {
+                delay(500)
+            }
+            startMainActivity(isFirstRun)
+        }
+    }
+
+    private fun startMainActivity(isFirstRun: Boolean) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("isFirstRun" , isFirstRun)
+        startActivity(intent)
+        finish()
+    }
+
+}
