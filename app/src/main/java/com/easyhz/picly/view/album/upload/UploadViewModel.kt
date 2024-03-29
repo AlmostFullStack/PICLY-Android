79fef664fc72b8fd1abc4b0e09b66941b2d31b4a
easyhz <jiton3@naver.com>
2024-03-30 02:03:21 +0900

    fix: 업로드 시 스레드 수정

--- app/src/main/java/com/easyhz/picly/view/album/upload/UploadViewModel.kt
+++ app/src/main/java/com/easyhz/picly/view/album/upload/UploadViewModel.kt
@@ -9,9 +9,11 @@ import com.easyhz.picly.domain.model.album.upload.gallery.GalleryImageItem
 import com.easyhz.picly.domain.usecase.album.upload.UploadUseCase
 import com.easyhz.picly.util.toFirebaseTimestamp
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -33,20 +35,23 @@ class UploadViewModel
         expiredTime: String,
         onFailure: (String) -> Unit,
         onSuccess: (String) -> Unit,
-    ) = viewModelScope.launch {
-        UserManager.currentUser?.uid?.let { ownerId ->
-            uploadUseCase.writeAlbums(
-                ownerId = ownerId,
-                tags = tags.value.orEmpty(),
-                selectedImageList = selectedImageList,
-                expireTime = "$expiredDate $expiredTime".toFirebaseTimestamp()
-            ).catch { e ->
-                e.localizedMessage?.let { onFailure(it) }
-            }.collectLatest {
+    ) = viewModelScope.launch(Dispatchers.IO) {
+        val ownerId = UserManager.currentUser?.uid ?: run {
+            return@launch
+        }
+        uploadUseCase.writeAlbums(
+            ownerId = ownerId,
+            tags = tags.value.orEmpty(),
+            selectedImageList = selectedImageList,
+            expireTime = "$expiredDate $expiredTime".toFirebaseTimestamp()
+        ).catch { e ->
+            e.localizedMessage?.let { onFailure(it) }
+        }.collectLatest {
+            withContext(Dispatchers.Main) {
                 onSuccess(it)
             }
-
         }
+
     }
 
     fun addTag(tag: String) {
