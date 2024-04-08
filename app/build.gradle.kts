05994be9f504822ce23266ec37b60480d3abae7b
easyhz <jiton3@naver.com>
2024-04-08 15:53:41 +0900

    fix: 터치 대상 크기 조정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.easyhz.picly"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
+        versionCode = 9
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
